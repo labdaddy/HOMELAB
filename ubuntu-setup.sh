870b8ab88451b70e1fc6b1acf17073f6aa625fac
labdaddy <47070992+labdaddy@users.noreply.github.com>
2024-01-08 21:14:18 -0800

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,51 +1,48 @@
-#example script (ubuntu)
+#!/bin/bash
 
-#first update the system
+#this script installs:
+#snapd
+#network manager
+#qemu agent
+#docker
+#cockpit
+
+#update the system
 sudo apt update && sudo apt upgrade -y
 
-#install qemu guest agent (for proxmox)
-sudo apt install qemu-guest-agent -y
+#install and enable snapd and create linkn to snap path
+sudo apt install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
 
-#enable guest agent on host
-sudo systemctl enable qemu-guest-agent
+#install & start network manager
+sudo snap install network-manager
+sudo systemctl start NetworkManager.service
 
-#install cockpit on ubuntu server
+#Set the default locale
+echo "Setting default locale..."
+locale-gen en_US.UTF-8
+
+#install and configure cockpit on ubuntu server
 sudo apt-get install cockpit -y
-
-#install podman (required)
 sudo apt-get install cockpit-podman -y
-
-#start, enable and check status of the cockpit service
 sudo systemctl start cockpit
 sudo systemctl enable cockpit
-sudo systemctl status cockpit
 
-#install cockpit on rocky server
-sudo dnf install cockpit -y
+#install and enable qemu guest agent
+sudo apt install qemu-guest-agent
+sudo systemctl enable qemu-guest-agent
 
-#start, enable and check status of the cockpit service
-sudo systemctl start cockpit.socket
-sudo systemctl enable cockpit.socket
-sudo systemctl status cockpit.socket
-
-#install docker
-#first remove any conflicting packages:
-for pkg in http://docker.io  docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-#then run the convenience script
+#install and enable docker engine
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 curl -fsSL https://get.docker.com -o get-docker.sh
-
-#then startup docker engine
-sudo sh ./get-docker.sh
+sudo sh get-docker.sh
 
 #make sure you can execute docker commands without typing sudo every damn time
 #create the docker group, add user to it and save the changes
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker
+sudo groupadd docker 
+sudo usermod -aG docker ${USER}
+su - ${USER}
+newgrp docker 
 
-#test run docker
-docker run hello-world
 
-#reboot system
-sudo reboot
